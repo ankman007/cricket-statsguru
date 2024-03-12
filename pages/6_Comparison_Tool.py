0c40deaaa840a16fbce4617107dfac4a04ff9216
Chirayu Rijal <chirayurijal@gmail.com>
2024-03-12 16:30:12 +0545

    one-to-one-comparison

--- pages/6_Comparison_Tool.py
+++ pages/6_Comparison_Tool.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from utilities import BattingStats, BowlingStats
+import pandas as pd
+import plotly.express as px
 
 st.set_page_config(
     page_title="Comparison Tool",
     page_icon="🎯",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
 
 )
 
@@ -23,8 +25,8 @@ def user_options(player_options):
     rating_type_options = ["Batting", "Bowling"]
     default_players = ["K Bhurtel", "RK Paudel", 'Sompal Kami', 'S Bhari', 'Aasif Sheikh', 'KS Airee']
     with st.sidebar: 
-        select_match_type = st.selectbox("Select Match Type", match_type_options)
-        select_rating_type = st.selectbox("Choose Rating Type", rating_type_options)
+        select_match_type = st.selectbox("Select Match Type", match_type_options , key = "match_type")
+        select_rating_type = st.selectbox("Choose Rating Type", rating_type_options , key= "rating_type")
     select_players = st.multiselect("Select Players To Compare", player_options, default_players, key="players")
 
     return select_players, select_match_type, select_rating_type
@@ -54,8 +56,73 @@ def multi_player_comparison():
         player_overview()
         show_data(batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20, selected_players, match_type, rating_type)
 
+
+def create_selectbox(series_1 , series_2):
+    #creating columns for the selectboxes 
+    column_1  , column_3 = st.columns(2)
+    with column_1: 
+        player_1 = st.selectbox(
+            "Select a player" , 
+            BowlingStats.my_union(series_1["Player"], series_2["Player"]) , 
+            key="Player_1"
+            )
+    # with column_2:
+    #     st.header("🆚")
+    with column_3:
+        
+        player_2 = st.selectbox(
+            "Select a player" , 
+            BowlingStats.my_union(series_1["Player"], series_2["Player"]) , 
+            key="Player_2")
+@st.cache_data
+def loading_info_data():
+    df = pd.read_csv("resources/PlayersInfo.csv")
+    return df       
+def showing_info():
+    data = loading_info_data()
+    column1 , column2 = st.columns(2)
+    with column1: 
+        st.image(data[data['Name'] == st.session_state.Player_1]['Photo'].iloc[0] , width=250)
+        st.markdown(f" **Age:** {int(data[data['Name'] == st.session_state.Player_1]['Age'].iloc[0])}")
+        st.markdown(f" **Batting Style:** {data[data['Name'] == st.session_state.Player_1]['Batting Style'].iloc[0]}")
+        st.markdown(f" **Bowling Style:** {data[data['Name'] == st.session_state.Player_1]['Bowling Style'].iloc[0]}")
+        st.markdown(f" **Playing Order:** {data[data['Name'] == st.session_state.Player_1]['Playing Order'].iloc[0]}")
+    with column2:
+        st.image(data[data['Name'] == st.session_state.Player_2]['Photo'].iloc[0] , width = 250)
+        st.markdown(f" **Age:** {int(data[data['Name'] == st.session_state.Player_2]['Age'].iloc[0])}")
+        st.markdown(f" **Batting Style:** {data[data['Name'] == st.session_state.Player_2]['Batting Style'].iloc[0]}")
+        st.markdown(f" **Bowling Style:** {data[data['Name'] == st.session_state.Player_2]['Bowling Style'].iloc[0]}")
+        st.markdown(f" **Playing Order:** {data[data['Name'] == st.session_state.Player_2]['Playing Order'].iloc[0]}")
+
+
+#creating pie charts for respective stats
+
+
+
+  
+def showing_data(batting_players_odi , bowling_players_odi , batting_players_t20 , bowling_players_t20):
+    column1 , column2 , column3 , column4 = st.columns(4)
+    with column1:
+        selected_data = batting_players_odi[(batting_players_odi["Player"] == st.session_state.Player_1) | (batting_players_odi["Player"] == st.session_state.Player_2)]
+        st.dataframe(selected_data.set_index("Player").T)
+
+    with column2:
+        selected_data = bowling_players_odi[(bowling_players_odi["Player"] == st.session_state.Player_1) | (bowling_players_odi["Player"] == st.session_state.Player_2)]
+        st.dataframe(selected_data.set_index("Player").T)
+    with column3:
+        selected_data = batting_players_t20[(batting_players_t20["Player"] == st.session_state.Player_1) | (batting_players_t20["Player"] == st.session_state.Player_2)]
+        st.dataframe(selected_data.set_index("Player").T)
+    with column4:
+        selected_data = bowling_players_t20[(bowling_players_t20["Player"] == st.session_state.Player_1) | (bowling_players_t20["Player"] == st.session_state.Player_2)]
+        st.dataframe(selected_data.set_index("Player").T)
+
+
 def one_on_one_comparision():
-    pass
+    batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20 = load_cricket_data()
+    create_selectbox(batting_players_odi , batting_players_t20)
+    showing_info()
+    showing_data(batting_players_odi , bowling_players_odi , batting_players_t20 , bowling_players_t20)
+        
 
 def main():
     st.markdown("<h1 style='text-align: center; color: black;'>Compare Cricketers: Batting, Bowling, Head-to-Head Records & Statistics</h1>", unsafe_allow_html=True)
