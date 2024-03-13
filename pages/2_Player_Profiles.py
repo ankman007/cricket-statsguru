d704a05774321af6c69db4b46642e3845da266ac
ankman007 <ankeetpoudel6@gmail.com>
2024-03-13 10:22:12 +0545

    design-changes-player-profile

--- pages/2_Player_Profiles.py
+++ pages/2_Player_Profiles.py
@@ -1,70 +1,77 @@
 import streamlit as st
-from utilities import BattingStats , BowlingStats
+from utilities import BattingStats, BowlingStats
 import pandas as pd  
 
 st.set_page_config(
     page_title="Player Profiles",
     page_icon="🏏",
-    layout="wide",
-    initial_sidebar_state="collapsed",
+    layout="centered",
+    initial_sidebar_state="expanded",
 )
-st.markdown("""
-    ### Welcome to the player profile!
-""")
 
-##Loading the data
 def loading_data():
     batting_players_odi, batting_players_t20 = BattingStats.load_data()
     bowling_players_odi, bowling_players_t20 = BowlingStats.load_bowling_data()
     bowling_players_odi, bowling_players_t20 = BowlingStats.set_column_names(bowling_players_odi, bowling_players_t20)
     bowling_players_odi, bowling_players_t20 = BowlingStats.fill_null_values(bowling_players_odi, bowling_players_t20)
     player_info = pd.read_csv("resources/PlayersInfo.csv")
-    return batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20 , player_info
+    return batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20, player_info
 
-##showing data in columns
-def show_data(batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20 , player_info ):
-    Players = player_info["Name"]
-    for idx , i in enumerate(Players):
-        main_container = st.container(border=True)
-        main_container.markdown(f"### {player_info[player_info['Name'] == Players[idx]]['Full name'].iloc[0]}")
-        column1 , column2 = st.columns(2)
-        with column1: 
-            main_container.image(player_info[player_info['Name'] == Players[idx]]['Photo'].iloc[0] , width=100)
-        with column2:
-            try: 
-                age = int(player_info[player_info['Name'] == Players[idx]]['Age'].iloc[0])
-            except:
-                age = "Unknown"
-            main_container.markdown(f" **Age:** {age}")
-            main_container.markdown(f" **Batting Style:** {player_info[player_info['Name'] == Players[idx]]['Batting Style'].iloc[0]}")
-            main_container.markdown(f" **Bowling Style:** {player_info[player_info['Name'] == Players[idx]]['Bowling Style'].iloc[0]}")
-            main_container.markdown(f" **Playing Order:** {player_info[player_info['Name'] == Players[idx]]['Playing Order'].iloc[0]}")
-            main_container.markdown(f" **District:** {player_info[player_info['Name'] == Players[idx]]['District'].iloc[0]}")
-            try:
-                main_container.markdown(f" **ODI Runs:** {int(batting_players_odi[batting_players_odi.Player == i]['Runs'].iloc[0])}")
-            except:
-                main_container.markdown(f" **ODI Runs:** {0}")
-            try:
-                main_container.markdown(f" **T20 Runs:** {int(batting_players_t20[batting_players_t20.Player == i]['Runs'].iloc[0])}")
-            except:
-                main_container.markdown(f" **T20 Runs:** {0}")
-            try:
-                main_container.markdown(f" **ODI Wickets:** {int(bowling_players_odi[bowling_players_odi.Player == i]['Wickets Taken'].iloc[0])}")
-            except:
-                main_container.markdown(f" **ODI Wickets:** {0}")
-            try:
-                main_container.markdown(f" **T20 Wickets:** {int(bowling_players_t20[bowling_players_t20.Player == i]['Wickets Taken'].iloc[0])}")
-            except:
-                main_container.markdown(f" **T20 Wickets:** {0}")
-            
-            
-            # st.markdown(f" **T20 Runs:** {int(batting_players_t20[batting_players_t20.Player == i]['Runs'].iloc[0])}")
+def display_player_info(player_info_row, batting_stats_odi, batting_stats_t20, bowling_stats_odi, bowling_stats_t20):
+    
+    total_runs = batting_stats_odi.get('Runs', 0) + batting_stats_t20.get('Runs', 0)
+    total_wickets = bowling_stats_odi.get('Wickets Taken', 0) + bowling_stats_t20.get('Wickets Taken', 0)
+    
+    with st.container(border=True):
+        # st.markdown(f"### {player_info_row['Full name']}", unsafe_allow_html=True)
+        st.markdown(f'<h2 style="text-align: center; color: #F9694B;">{player_info_row["Full name"]}</h2>', unsafe_allow_html=True)
+        st.markdown(f'<hr style="border-top: 2px solid #EF7D65;">', unsafe_allow_html=True)
 
+        col1, col2 = st.columns([1, 1])  
+        
+        with col1:
+            st.image(player_info_row['Photo'], width=250)
+        
+        with col2:
+            st.markdown(f" **Age:** {player_info_row.get('Age', 'Unknown')}  \n"
+                        f" **Batting Style:** {player_info_row.get('Batting Style', '')}  \n"
+                        f" **Bowling Style:** {player_info_row.get('Bowling Style', '')}  \n"
+                        f" **Playing Order:** {player_info_row.get('Playing Order', '')}  \n"
+                        f" **District:** {player_info_row.get('District', '')}  \n"
+                        f" **Total Runs:** {total_runs}  \n"
+                        f" **Total Wickets Taken:** {total_wickets}  \n")
+        with st.expander("Player Bio"):
+            st.write('Player bio is being added soon. Stay tuned.😉')
 
 def main():
-    batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20 , player_info = loading_data()
-    show_data( batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20 , player_info)
-
+    batting_players_odi, batting_players_t20, bowling_players_odi, bowling_players_t20, player_info = loading_data()
+    
+    for _, player_row in player_info.iterrows():
+        player_name = player_row['Name']
+        
+        if not batting_players_odi[batting_players_odi['Player'] == player_name].empty:
+            batting_stats_odi = batting_players_odi[batting_players_odi['Player'] == player_name].iloc[0]
+        else:
+            batting_stats_odi = {'Runs': 0}  
+        
+        if not batting_players_t20[batting_players_t20['Player'] == player_name].empty:
+            batting_stats_t20 = batting_players_t20[batting_players_t20['Player'] == player_name].iloc[0]
+        else:
+            batting_stats_t20 = {'Runs': 0}  
+        
+        if not bowling_players_odi[bowling_players_odi['Player'] == player_name].empty:
+            bowling_stats_odi = bowling_players_odi[bowling_players_odi['Player'] == player_name].iloc[0]
+        else:
+            bowling_stats_odi = {'Wickets Taken': 0}  
+        
+        if not bowling_players_t20[bowling_players_t20['Player'] == player_name].empty:
+            bowling_stats_t20 = bowling_players_t20[bowling_players_t20['Player'] == player_name].iloc[0]
+        else:
+            bowling_stats_t20 = {'Wickets Taken': 0}  
+        
+        display_player_info(player_row, batting_stats_odi, batting_stats_t20, bowling_stats_odi, bowling_stats_t20)
 
 if __name__ == "__main__":
     main()
+
+
